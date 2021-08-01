3e82b47deb91f8ac43f1d540c099c21add1183ee
humanpro2k <87627063+humanpro2k@users.noreply.github.com>
2021-08-01 15:47:31 +0700

    Update settings.py

--- WatchMate/WatchMate/settings.py
+++ WatchMate/WatchMate/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*!u#rs+1t&3+y6te_ppzv^kdse4(7o=r1ajpomfe*9vee2eilf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
